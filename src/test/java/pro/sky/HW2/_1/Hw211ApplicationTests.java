4de1b59705587e0874dea2b1c2efc7b3e637b80c
Ulyana Shandruk <ulyana.shandruk@gmail.com>
2025-08-14 18:15:33 +0300

    Первый коммит.

---
+++ src/test/java/pro/sky/HW2/_1/Hw211ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HW2._1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
