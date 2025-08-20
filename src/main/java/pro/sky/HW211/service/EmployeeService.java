7ce3282d5e252d1b3ebcb9ee508cfd29730ab7d1
Ulyana Shandruk <ulyana.shandruk@gmail.com>
2025-08-20 20:02:11 +0300

    Шаг 4 - Создать сервис EmployeeService, который хранит внутри поле с коллекцией сотрудников и константу хранящее максимальное возможное количество сотрудников в фирме.

---
+++ src/main/java/pro/sky/HW211/service/EmployeeService.java
@@ -0,0 +1,68 @@
+package pro.sky.HW211.service;
+import org.springframework.stereotype.Service;
+import pro.sky.HW211.model.Employee;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class EmployeeService {
+    // Записи о сотрудниках
+    private List<Employee> listEmployees;
+
+    // Максимальное возможное количество сотрудников в фирме.
+    private final int maxCountOfEmployee = 20;
+
+    // Конвертировать List<Employee> в List<String>
+    private List<String> convertListEmployeesToListString(List<Employee> listInput)
+    {
+        List<String>listOutput = new ArrayList<>();
+        for (Employee employee : listInput)
+        {
+            String e = employee.toStringAll();
+            listOutput.add(e);
+        }
+
+        return listOutput;
+    }
+
+    // Конструктор
+    public EmployeeService() {
+        this.listEmployees = new ArrayList<Employee>(); // null;
+        fill();
+    }
+
+    // Заполнить список сотрудников
+    private void fill()
+    {
+        listEmployees.add(new Employee(4, "Иван", "Иванов"));
+        listEmployees.add(new Employee(3, "Иван", "Иванов"));
+        listEmployees.add(new Employee(2, "Екатерина", "Павлова"));
+        listEmployees.add(new Employee(1, "Марина", " Антонова"));
+        listEmployees.add(new Employee(5, "Семён", "Сергеев"));
+       /* arrayEmployees[5] = new Employee("Иванов Александр Иванович", 1, 40000);
+        arrayEmployees[6] = new Employee("Петров Семён Александрович", 3, 50000);
+        arrayEmployees[7] = new Employee("Семёнова Елена Ивановна", 2, 60000);
+        arrayEmployees[8] = new Employee("Антонова Марина Сергеевна", 4, 80000);
+        arrayEmployees[9] = new Employee("Сергеев Семён Алексеевич", 1, 50000);*/
+    }
+
+    // Получить список всех сотрудников
+    private List<Employee> getListOfEmployeesAll()
+    {
+        List<Employee> listOfEmployeesAll = listEmployees.stream().filter(e -> e != null).toList();
+        return listOfEmployeesAll;
+    }
+
+    // Получить список всех сотрудников (список строк, для браузера)
+    private List<String> getListStringOfEmployeesAll()
+    {
+        List<Employee> listInput = getListOfEmployeesAll();
+        List<String> listOutput = convertListEmployeesToListString(listInput);
+        return listOutput;
+    }
+
+
+
+
+}
