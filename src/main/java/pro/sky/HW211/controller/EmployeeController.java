ff52ca49e77d298f17c6d5534f147b9090dad0f6
Ulyana Shandruk <ulyana.shandruk@gmail.com>
2025-08-22 20:34:22 +0300

    Шаг 11 - Объявить в контроллере 3 метода (добавить, удалить, найти) - черновик.

--- src/main/java/pro/sky/HW211/controller/EmployeeController.java
+++ src/main/java/pro/sky/HW211/controller/EmployeeController.java
@@ -1,6 +1,12 @@
 package pro.sky.HW211.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.HW211.model.Employee;
 import pro.sky.HW211.service.EmployeeService;
+
+import java.time.LocalDateTime;
 
 
 @RestController
@@ -10,5 +16,43 @@ public class EmployeeController {
     // Конструктор
     public EmployeeController(EmployeeService counterService) {
         this.employeeService = counterService;
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello, world!" + "\\r\\n " + LocalDateTime.now();
+    }
+
+    // http://localhost:8080/greetings?name=Лев&lastName=Толстой // "Здравствуйте, Лев Толстой!";
+    @GetMapping("/greetings")
+    public String greetings(@RequestParam("name") String name, @RequestParam("lastName") String lastName) {
+        return "Здравствуйте, " + name + " " + lastName + "!";
+    }
+/*
+    // http://localhost:8080/greetings?name=Лев&lastName=Толстой // "Здравствуйте, Лев Толстой!";
+    @GetMapping("/greetings/add")
+    public String greetings_add(@RequestParam("name") String name, @RequestParam("lastName") String lastName) {
+        return "Здравствуйте, " + name + " " + lastName + "!";
+    }
+*/
+
+
+    // http://localhost:8080/employee/add?firstName=Лев&lastName=Толстой // {"firstName": "Лев", "lastName": "Толстой"}
+    //@GetMapping("/employee/add") - не работает
+    @GetMapping("/employeeadd")
+    public String getEmployeeByName(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) throws Exception
+    {
+        String result = employeeService.addEmployee(firstName, lastName);
+        return result;
+    }
+
+
+    // http://localhost:8080/employee/remove?firstName=Лев&lastName=Толстой // {"firstName": "Лев", "lastName": "Толстой"}
+    //@GetMapping("/employee/remove") - не работает?
+    @GetMapping("/employeeremove")
+    public String removeEmployeeByName(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) throws Exception
+    {
+        String result = employeeService.removeEmployee(firstName, lastName);
+        return result;
     }
 }
--- src/main/java/pro/sky/HW211/service/EmployeeService.java
+++ src/main/java/pro/sky/HW211/service/EmployeeService.java
@@ -13,7 +13,7 @@ public class EmployeeService {
     private List<Employee> listEmployees;
 
     // Максимальное возможное количество сотрудников в фирме.
-    private final int maxCountOfEmployee = 20;
+    private final int maxCountOfEmployee = 10;
 
     // Конвертировать List<Employee> в List<String>
     private List<String> convertListEmployeesToListString(List<Employee> listInput)
@@ -28,6 +28,24 @@ public class EmployeeService {
         return listOutput;
     }
 
+    // Найти сотрудника
+    public Employee findEmployee(Employee employeeToFind)
+    {
+        //boolean isContains = listEmployees.contains(employeeToFind); // не работает
+        List<Employee> listFoundedEmployees = listEmployees.stream().filter(e -> e.equals(employeeToFind)).toList();
+
+        //List<Employee> listFoundedEmployees = listEmployees.stream().filter(e -> e.getFirstName() == employeeToFind.getFirstName()).filter(e -> e.getLastName() == employeeToFind.getLastName()).toList();
+        //List<Employee> tt1 = listEmployees.stream().filter(e -> e.getFirstName() == employeeToFind.getFirstName()).toList();
+        //List<Employee> tt2 = tt1.stream().filter(e -> e.getLastName() == employeeToFind.getLastName()).toList();
+
+            if (listFoundedEmployees.size() > 0)
+            {
+                return listFoundedEmployees.get(0);
+            }
+
+        return null;
+    }
+
     // Конструктор
     public EmployeeService() {
         this.listEmployees = new ArrayList<Employee>(); // null;
@@ -37,11 +55,11 @@ public class EmployeeService {
     // Заполнить список сотрудников
     private void fill()
     {
-        listEmployees.add(new Employee(4, "Иван", "Иванов"));
-        listEmployees.add(new Employee(3, "Иван", "Иванов"));
-        listEmployees.add(new Employee(2, "Екатерина", "Павлова"));
-        listEmployees.add(new Employee(1, "Марина", " Антонова"));
-        listEmployees.add(new Employee(5, "Семён", "Сергеев"));
+        listEmployees.add(new Employee("Иван", "Иванов"));
+        listEmployees.add(new Employee("Иван", "Иванов"));
+        listEmployees.add(new Employee("Екатерина", "Павлова"));
+        listEmployees.add(new Employee("Марина", " Антонова"));
+        listEmployees.add(new Employee("Семён", "Сергеев"));
        /* arrayEmployees[5] = new Employee("Иванов Александр Иванович", 1, 40000);
         arrayEmployees[6] = new Employee("Петров Семён Александрович", 3, 50000);
         arrayEmployees[7] = new Employee("Семёнова Елена Ивановна", 2, 60000);
@@ -64,43 +82,8 @@ public class EmployeeService {
         return listOutput;
     }
 
-    // Добавить нового сотрудника
-    public String addEmployee(Employee employeeToAdd) throws Exception
-    {
-        boolean isEmployeeToAddExist = listEmployees.contains(employeeToAdd);
-        if(isEmployeeToAddExist)
-        {
-            throw new EmployeeAlreadyAddedException("");
-        }
-        if(listEmployees.size() >= maxCountOfEmployee)
-        {
-            throw new IndexOutOfBoundsException("Коллекция переполнена.");
-        }
-        listEmployees.add(employeeToAdd);
-        boolean isAddedSuccessful = listEmployees.contains(employeeToAdd);
-        if(isAddedSuccessful)
-            return ("Сотрудник " + employeeToAdd.getId() + " был успешно добавлен.");
-        return "Сотрудник не добавлен.";
-    }
-
-    // Удалить сотрудника
-    public String deleteEmployee(int employeeToDelId) throws Exception
-    {
-        Employee employeeToDel = findEmployeeById(employeeToDelId);
-        boolean isEmployeeToDelExist = listEmployees.contains(employeeToDel);
-        if(!isEmployeeToDelExist)
-        {
-           throw new EmployeeNotFoundException("");
-        }
-        listEmployees.remove(employeeToDel);
-        boolean isExist = listEmployees.contains(employeeToDel);
-        if(!isExist)
-            throw new EmployeeNotFoundException("");
-        return ("Сотрудник успешно удалён.");
-    }
-
     // Найти сотрудника по его id
-    public Employee findEmployeeById(int employeeId) throws Exception
+    private Employee privateFindEmployeeById(int employeeId) throws Exception
     {
         //Arrays.stream(arrayEmployees).filter(emp -> emp.getId() == employeeId);
         for (Employee e : listEmployees) {
@@ -116,4 +99,58 @@ public class EmployeeService {
         return null;
     }
 
+    // Добавить нового сотрудника
+    public String addEmployee(String firstName, String lastName) throws Exception
+    {
+        Employee employeeToAdd = new Employee(firstName, lastName);
+        //boolean isEmployeeToAddExist = listEmployees.contains(employeeToAdd); // не работает
+        Employee foundedEmployee = findEmployee(employeeToAdd);
+        if(foundedEmployee != null)
+        {
+            throw new EmployeeAlreadyAddedException("");
+        }
+        if(listEmployees.size() >= maxCountOfEmployee)
+        {
+            throw new IndexOutOfBoundsException("Коллекция переполнена.");
+        }
+        listEmployees.add(employeeToAdd);
+        boolean isAddedSuccessful = listEmployees.contains(employeeToAdd);
+        if(isAddedSuccessful)
+            return employeeToAdd.toJSON(); //("Сотрудник успешно добавлен.");
+        return "Сотрудник не добавлен.";
+    }
+
+    // Удалить сотрудника
+    public String removeEmployee(String firstName, String lastName) throws Exception
+    {
+        Employee employeeToDel = new Employee(firstName, lastName);
+        //boolean isEmployeeToDelExist = listEmployees.contains(employeeToDel);
+        Employee foundedEmployee = findEmployee(employeeToDel);
+        if(foundedEmployee != null)
+        {
+           throw new EmployeeNotFoundException("");
+        }
+        listEmployees.remove(employeeToDel);
+        boolean isExist = listEmployees.contains(employeeToDel);
+        if(!isExist)
+            throw new EmployeeNotFoundException("");
+        return employeeToDel.toJSON(); //("Сотрудник успешно удалён.");
+    }
+
+    // Найти сотрудника по его id
+    public String findEmployeeById(int employeeId) throws Exception
+    {
+        //Arrays.stream(arrayEmployees).filter(emp -> emp.getId() == employeeId);
+        for (Employee e : listEmployees) {
+            if (e.getId() == employeeId)
+            {
+                return e.toJSON();
+            }
+            else
+            {
+                throw new EmployeeNotFoundException("");
+            }
+        }
+        return null;
+    }
 }
